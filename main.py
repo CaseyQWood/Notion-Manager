ee415b9bc17fe0f2d998c0966bc2170e2c535714
casey.Q.Wood <caseywood252@gmail.com>
2024-02-10 01:34:40 -0800

    clean up code, add api call

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import Optional
 from openai import OpenAI
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 openai_organization = os.getenv("OPENAI_ORGANIZATION")
-
-print(openai_api_key)
-print(openai_organization)
 
 client = OpenAI(api_key=openai_api_key, organization=openai_organization)
 
@@ -27,6 +24,21 @@ app.add_middleware(
 )
 
 
-@app.get("/")
+class Message(BaseModel):
+    role: str
+    content: str
+    function_call: Optional[str] = None
+    tool_calls: Optional[str] = None
+
+
+@app.get("/chat-completion")
 async def root():
+
+    # response: Message = client.chat.completions.create(
+    #     model="gpt-4-turbo-preview",
+    #     messages=convo.get_messages(),
+    #     tools=tools,
+    #     tool_choice="auto",
+    # )
+
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
