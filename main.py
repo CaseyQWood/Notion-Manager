5f81ad7f59772c196147b099af074738e03de2b8
casey.Q.Wood <caseywood252@gmail.com>
2024-02-10 01:09:38 -0800

    installed initial packages

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_organization = os.getenv("OPENAI_ORGANIZATION")
+
+print(openai_api_key)
+print(openai_organization)
+
+client = OpenAI(api_key=openai_api_key, organization=openai_organization)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 @app.get("/")
 async def root():
